c8b504f922b6333d8a9152756c57f4af9302c59c
danroot <droot@lifecycle-solutions.com>
2013-09-25 10:03:17 -0500

    nuget

--- SimpleEntityApi.NuGet/copypackandpush.ps1
+++ SimpleEntityApi.NuGet/copypackandpush.ps1
@@ -1,4 +1,7 @@
-﻿copy ..\SimpleEntityApi.Web\Scripts\SimpleEntityApi\SimpleEntityApi-0.1.js .\content\Scripts
+﻿copy ..\SimpleEntityApi.Web\Scripts\SimpleEntityApi\SimpleEntityApi.js .\content\Scripts
+copy ..\SimpleEntityApi.Web\Scripts\SimpleEntityApi\SimpleEntityApi.min.js .\content\Scripts
+copy ..\SimpleEntityApi.Web\Scripts\SimpleEntityApi\SimpleEntityApi.angular.js .\content\Scripts
+copy ..\SimpleEntityApi.Web\Scripts\SimpleEntityApi\SimpleEntityApi.angular.min.js .\content\Scripts
 copy ..\SimpleEntityApi.Web\bin\SimpleEntityApi.Library.dll .\lib\net40
 
 del *.nupkg 
